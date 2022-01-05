5f26b39260915bf890b4b3717afe2193423e217d
UlanbekRasul <ulanbekrasul@gmail.com>
2022-01-05 16:31:14 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw33/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.hw33;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArrayList<Model> names = new ArrayList<>();
+    private itemAdapter adapter;
+    private Object TextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView = findViewById(R.id.textView);
+        LoadData();
+        adapter = new itemAdapter(names);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void LoadData() {
+
+        names.add(new Model(R.drawable.ic_circle, "Sagynaly \n 0555806961"));
+    }
+}
